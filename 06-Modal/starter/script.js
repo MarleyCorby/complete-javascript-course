82cf5e1696cbb4e05703e15bc2214147d5943562
Nana Tsimintia <ntsimintia@impel.io>
2024-01-22 15:33:54 +0400

    added a functionality on overlay click

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,6 +6,8 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
+
+    //button functionality on modal and on x onClick
 for (let i =0; i < btnsOpenModal.length; i++)
 console.log(btnsOpenModal[i].addEventListener
     ('click', function(){
@@ -13,7 +15,11 @@ console.log(btnsOpenModal[i].addEventListener
         overlay.classList.remove('hidden')
     }))
 
-btnCloseModal.addEventListener('click', function(){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-});
+    const closeModal = function(){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    }
+btnCloseModal.addEventListener('click', closeModal );
+
+    //functionality on overlay(clicking outside of modal to close it) click for closing modal
+overlay.addEventListener('click', closeModal);
