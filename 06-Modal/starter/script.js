1b9d2273bfd648f5009d0c5050f6dd0944aa282c
Nana Tsimintia <ntsimintia@impel.io>
2024-01-22 15:26:36 +0400

    modal close open on click

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+for (let i =0; i < btnsOpenModal.length; i++)
+console.log(btnsOpenModal[i].addEventListener
+    ('click', function(){
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden')
+    }))
+
+btnCloseModal.addEventListener('click', function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
