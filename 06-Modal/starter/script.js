01b22002218a8d920d0449f45c1b213111e3f2f3
MarleyCorby <112287260+MarleyCorby@users.noreply.github.com>
2024-01-25 22:43:16 +0400

    added a closing on escape keydown

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,20 +6,30 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
+//button functionality on modal and on x onClick
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].addEventListener('click', showModal));
 
-    //button functionality on modal and on x onClick
-const showModal = function(){
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden')
-}
-for (let i =0; i < btnsOpenModal.length; i++)
-console.log(btnsOpenModal[i].addEventListener('click', showModal))
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
 
-const closeModal = function(){
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    }
-btnCloseModal.addEventListener('click', closeModal );
-
-    //functionality on overlay(clicking outside of modal to close it) click for closing modal
+//functionality on overlay(clicking outside of modal to close it) click for closing modal
 overlay.addEventListener('click', closeModal);
+
+//close the modal on keydown; the functino(e) shows us in console which key is being press (e.key shows us exact key name)
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      console.log('Esc was pressed!');
+      closeModal();
+    }
+  }
+});
