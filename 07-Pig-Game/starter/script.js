93cc3246450096cb50f4b99ae62aa575c5f1c2b2
MarleyCorby <112287260+MarleyCorby@users.noreply.github.com>
2024-01-27 01:40:39 +0400

    background color switch for player--active

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,9 +1,13 @@
 'use strict';
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 
 const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 let currentScore = 0;
 let activePlayer = 0;
@@ -32,6 +36,18 @@ btnRoll.addEventListener('click', function () {
     console.log(currentScore);
   } else {
     //switch to the next player
+    //turning previous player's currentScore to 0 before switching to a next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    //actually swittching to a next player!
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
+});
+
+//HOLD
+btnHold.addEventListener('click', function () {
+  //onclicik currentScore = score--1 or score--0
+  console.log('hold was pressed');
+  score0El.textContent = currentScore;
 });
